619f9275f2a4ac55c03ebb15ba19ab501495a8d3
clean_brain <clean_brain@nandsoft.co.kr>
2023-11-17 11:31:52 +0900

    first commit

---
+++ src/main/java/com/example/StuSpringJpaWithQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuSpringJpaWithQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuSpringJpaWithQuerydslApplication.class, args);
+	}
+
+}
